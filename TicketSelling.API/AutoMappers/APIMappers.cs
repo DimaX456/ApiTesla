7a4c8c3c46fde24ff88fd504898ffdd6bdad3c65
DimaX456 <slgwtvshow@mail.ru>
2023-12-21 12:53:04 +0300

    123

---
+++ TicketSelling.API/AutoMappers/APIMappers.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using AutoMapper.Extensions.EnumMapping;
+using TicketSelling.API.Enums;
+using TicketSelling.API.Models.CreateRequest;
+using TicketSelling.API.Models.Response;
+using TicketSelling.Services.Contracts.Enums;
+using TicketSelling.Services.Contracts.Models;
+using TicketSelling.Services.Contracts.ModelsRequest;
+
+namespace TicketSelling.API.AutoMappers
+{
+    /// <summary>
+    /// Маппер
+    /// </summary>
+    public class APIMappers : Profile
+    {
+        public APIMappers()
+        {
+            CreateMap<PostModel, PostResponse>().ConvertUsingEnumMapping(opt => opt.MapByName()).ReverseMap();
+
+            CreateMap<CreateCinemaRequest, CinemaModel>(MemberList.Destination)
+                .ForMember(x => x.Id, opt => opt.Ignore());
+
+            CreateMap<CreateFilmRequest, FilmModel>(MemberList.Destination)
+                .ForMember(x => x.Id, opt => opt.Ignore());
+
+            CreateMap<CreateHallRequest, HallModel>(MemberList.Destination)
+                .ForMember(x => x.Id, opt => opt.Ignore());
+
+            CreateMap<CreateClientRequest, ClientModel>(MemberList.Destination)
+                .ForMember(x => x.Id, opt => opt.Ignore());
+
+            CreateMap<CreateStaffRequest, StaffModel>(MemberList.Destination)
+                .ForMember(x => x.Id, opt => opt.Ignore());
+
+            CreateMap<CinemaRequest, CinemaModel>(MemberList.Destination);
+            CreateMap<FilmRequest, FilmModel>(MemberList.Destination);
+            CreateMap<HallRequest, HallModel>(MemberList.Destination);
+            CreateMap<ClientRequest, ClientModel>(MemberList.Destination);
+            CreateMap<StaffRequest, StaffModel>(MemberList.Destination);
+            CreateMap<TicketRequest, TicketModel>(MemberList.Destination)
+                .ForMember(x => x.Hall, opt => opt.Ignore())
+                .ForMember(x => x.Cinema, opt => opt.Ignore())
+                .ForMember(x => x.Client, opt => opt.Ignore())
+                .ForMember(x => x.Film, opt => opt.Ignore())
+                .ForMember(x => x.Staff, opt => opt.Ignore());
+
+            CreateMap<TicketRequest, TicketRequestModel>(MemberList.Destination);
+            CreateMap<CreateTicketRequest, TicketRequestModel>(MemberList.Destination)
+                .ForMember(x => x.Id, opt => opt.Ignore());
+
+            CreateMap<FilmModel, FilmResponse>(MemberList.Destination);
+            CreateMap<HallModel, HallResponse>(MemberList.Destination);
+            CreateMap<CinemaModel, CinemaResponse>(MemberList.Destination);
+            CreateMap<TicketModel, TicketResponse>(MemberList.Destination);
+            CreateMap<StaffModel, StaffResponse>(MemberList.Destination)
+                .ForMember(x => x.Name, opt => opt.MapFrom(src => $"{src.LastName} {src.FirstName} {src.Patronymic}"));
+
+            CreateMap<ClientModel, ClientResponse>(MemberList.Destination)
+                .ForMember(x => x.Name, opt => opt.MapFrom(src => $"{src.LastName} {src.FirstName} {src.Patronymic}"));
+        }
+    }
+}
---
+++ TicketSelling.Services.Tests/TestDataGenerator.cs
@@ -0,0 +1,182 @@
+﻿using TicketSelling.Context.Contracts.Models;
+using TicketSelling.Services.Contracts.Models;
+using TicketSelling.Services.Contracts.ModelsRequest;
+
+namespace TicketSelling.Services.Tests
+{
+    internal static class TestDataGenerator
+    {
+        static internal Cinema Cinema(Action<Cinema>? settings = null)
+        {
+            var result = new Cinema
+            {
+                Title = $"{Guid.NewGuid():N}",
+                Address = $"{Guid.NewGuid():N}"
+            };
+            result.BaseAuditSetParamtrs();
+
+            settings?.Invoke(result);
+            return result;
+        }
+
+        static internal Film Film(Action<Film>? settings = null)
+        {
+            var result = new Film
+            {
+                Title = $"{Guid.NewGuid():N}",
+                Description = $"{Guid.NewGuid():N}"
+            };
+            result.BaseAuditSetParamtrs();
+
+            settings?.Invoke(result);
+            return result;
+        }
+
+        static internal Hall Hall(Action<Hall>? settings = null)
+        {
+            var result = new Hall
+            {
+                Number = 1,
+                NumberOfSeats = 20
+            };
+            result.BaseAuditSetParamtrs();
+
+            settings?.Invoke(result);
+            return result;
+        }
+
+        static internal Client Client(Action<Client>? settings = null)
+        {
+            var result = new Client
+            {
+                FirstName = $"{Guid.NewGuid():N}",
+                LastName = $"{Guid.NewGuid():N}",
+                Patronymic = $"{Guid.NewGuid():N}",
+                Age = 20,
+                Email = $"{Guid.NewGuid():N}"
+            };
+            result.BaseAuditSetParamtrs();
+
+            settings?.Invoke(result);
+            return result;
+        }
+
+        static internal Staff Staff(Action<Staff>? settings = null)
+        {
+            var result = new Staff
+            {
+                FirstName = $"{Guid.NewGuid():N}",
+                LastName = $"{Guid.NewGuid():N}",
+                Patronymic = $"{Guid.NewGuid():N}",
+                Age = 20,
+                Post = Context.Contracts.Enums.Post.Manager
+            };
+            result.BaseAuditSetParamtrs();
+
+            settings?.Invoke(result);
+            return result;
+        }
+
+        static internal Ticket Ticket(Action<Ticket>? settings = null)
+        {
+            var result = new Ticket
+            {
+                Date = DateTimeOffset.Now,
+                Place = 1,
+                Row = 1,
+                Price = 100
+            };
+            result.BaseAuditSetParamtrs();
+
+            settings?.Invoke(result);
+            return result;
+        }
+
+        static internal CinemaModel CinemaModel(Action<CinemaModel>? settings = null)
+        {
+            var result = new CinemaModel
+            {
+                Id = Guid.NewGuid(),
+                Title = $"{Guid.NewGuid():N}",
+                Address = $"{Guid.NewGuid():N}"
+            };
+
+            settings?.Invoke(result);
+            return result;
+        }
+
+        static internal FilmModel FilmModel(Action<FilmModel>? settings = null)
+        {
+            var result = new FilmModel
+            {
+                Id = Guid.NewGuid(),
+                Title = $"{Guid.NewGuid():N}",
+                Description = $"{Guid.NewGuid():N}",
+                Limitation = 16
+            };
+
+            settings?.Invoke(result);
+            return result;
+        }
+
+        static internal HallModel HallModel(Action<HallModel>? settings = null)
+        {
+            var result = new HallModel
+            {
+                Id = Guid.NewGuid(),
+                Number = 1,
+                NumberOfSeats = 20
+            };
+
+            settings?.Invoke(result);
+            return result;
+        }
+
+        static internal ClientModel ClientModel(Action<ClientModel>? settings = null)
+        {
+            var result = new ClientModel
+            {
+                Id = Guid.NewGuid(),
+                FirstName = $"{Guid.NewGuid():N}",
+                LastName = $"{Guid.NewGuid():N}",
+                Patronymic = $"{Guid.NewGuid():N}",
+                Age = 18,
+                Email = "kochetkov@gmail.com"
+            };
+
+            settings?.Invoke(result);
+            return result;
+        }
+
+        static internal StaffModel StaffModel(Action<StaffModel>? settings = null)
+        {
+            var result = new StaffModel
+            {
+                Id = Guid.NewGuid(),
+                FirstName = $"{Guid.NewGuid():N}",
+                LastName = $"{Guid.NewGuid():N}",
+                Patronymic = $"{Guid.NewGuid():N}",
+                Age = 18,
+                Post = Contracts.Enums.PostModel.None
+            };
+
+            settings?.Invoke(result);
+            return result;
+        }
+
+        static internal TicketRequestModel TicketRequestModel(Action<TicketRequestModel>? settings = null)
+        {
+            var result = new TicketRequestModel
+            {
+                Id = Guid.NewGuid(),
+                Date = DateTimeOffset.Now.AddDays(1),
+                Place = 1,
+                Row = 1,
+                Price = 100
+            };
+
+            settings?.Invoke(result);
+            return result;
+        }
+    }
+}
